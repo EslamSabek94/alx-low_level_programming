97fe9777bc09694fbc875401553c840413a13e77
EslamSabek94 <eslamsabek94@gmail.com>
2023-10-06 00:00:29 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
-/*
- * main - print a qoute using write function
- * write (int fd.const void *buf.size_t count)
+/**
+ * main - a c program that prints a line to the stanfard error
  * Return: 1 (not success)
 */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1.quo.59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
