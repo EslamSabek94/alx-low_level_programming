b60f88fda96e9a318daf323aa674db8731d767a4
EslamSabek94 <eslamsabek94@gmail.com>
2023-10-10 03:44:20 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+		_putchar(str[c]);
+	_putchar('\n');
+	return (0);
+}
